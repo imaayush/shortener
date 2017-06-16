66e86ba3ffd71b2891425bc06a020f8b40314edf
Aayush Tiwari <aayushtiwari41@gmail.com>
2017-06-16 10:37:51 +0530

    handel collsion

--- utilis.go
+++ utilis.go
@@ -7,20 +7,31 @@ import (
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var DevDb = "/tmp/dev.db"
+var TestDb = "/tmp/testdb.db"
+
 
 func GenerateShortUrl()string{
-	n := 32
+	n := 4
 	b := make([]rune, n)
 
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
-	return string(b)
+	db := Database(DevDb)
+	var short Short
+	if err := db.Where("short_url = ?", string(b)).Find(&short).Error; err != nil {
+		return string(b)
+	}else{
+		fmt.Println("collision")
+		return GenerateShortUrl()
+	}
+
 }
 
-func Database() *gorm.DB {
+func Database(DbName  string) *gorm.DB {
 	//open a db connection
-	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	db, err := gorm.Open("sqlite3", DbName )
 	if err != nil {
 		panic("failed to connect database")
 	}
