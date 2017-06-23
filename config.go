096e71471352091a4513597a368c00dff9e13619
Aayush Tiwari <aayushtiwari41@gmail.com>
2017-06-23 16:13:17 +0530

    added config file

---
+++ config.go
@@ -0,0 +1,7 @@
+package main
+
+var Db = "sqlite3"
+var SlugLength = 4
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+//var DbConfig = "host=127.0.0.1 user=short dbname=short sslmode=disable password=123456"
+var DbConfig = "/tmp/test1.db"
